8275445d64544680abfb3e152b8cd51961353e35
JefarAberson19 <onepiecehaki215@gmail.com>
2020-05-04 16:49:42 +0800

    Update by napa
    updated my napa

---
+++ 13_Aniq_PA4/Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("LoadScene", 1f);
+    }
+
+    void LoadScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
